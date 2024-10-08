0634d9c5ab3814c668e59969d4186b13f883a6d5
Muzamil Adnan <muzamil.adnan@pasajebus.com>
2024-10-08 11:57:59 +0530

    check aith route added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import {signup,login,logout,googleSignIn} from '../controllers/auth.controller.js'
+import protectRoute from '../middlewares/protectedRoute.js'
 const router = express.Router()
 
 router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
 router.post('/google', googleSignIn);
+router.get('/check-auth', protectRoute, (req, res) => {
+    // If protectRoute middleware allows the request to proceed, the user is authenticated
+    res.status(200).json({ isAuthenticated: true, user: req.user });
+  });
 
 
 export default router;
